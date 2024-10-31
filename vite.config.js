98b9352489cbbb6f315205f97361401f0f8f74da
Riley Oest <95721769+roest1@users.noreply.github.com>
2024-10-30 21:15:46 -0500

    changed vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
+// https://vite.dev/config/
 export default defineConfig({
-     base: "/example_ui/",
-     plugins: [react()],
-});
+  plugins: [react()],
+})
